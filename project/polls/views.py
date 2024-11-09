bf434180d3c6dbd19599123a185379b4f95e7ba2
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-09 13:33:18 +0200

    create view for question results

--- project/polls/views.py
+++ project/polls/views.py
@@ -17,7 +17,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
