df017be3c26b63dc0d6b3a3f614e4ff26f8ae577
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-10 22:34:08 +0200

    make admin question list representation better

--- project/polls/admin.py
+++ project/polls/admin.py
@@ -9,6 +9,10 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ["question_text"]
+    list_filter = ["pub_date"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
