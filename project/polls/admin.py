cae5433969cb989e620a0d14ebfbf5cf6f536511
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-10 22:21:08 +0200

    make admin question representation better

--- project/polls/admin.py
+++ project/polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from . import models
+from .models import Choice, Question
 
-admin.site.register(models.Question)
-admin.site.register(models.Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
